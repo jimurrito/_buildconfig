3206c70dae26078ee10b87b52904e0d8ebcfb487
jimurrito <jimurrito@gmail.com>
2024-01-21 18:49:55 -0800

    did a thing or two maybe

---
+++ scripts/install_elixir_deb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /usr/bin
+
+wget https://github.com/elixir-lang/elixir/archive/refs/tags/v1.15.7.tar.gz | tar -xf
+rm -rf  v1.15.7.tar.gz
+
+cd /usr/bin/elixir-1.15.7
+make clean compile
+
+# Add bins to path
+cd
+printf "\nexport PATH=%s':/usr/bin/elixir-1.15.7/bin'\n" $PATH > .bashrc
